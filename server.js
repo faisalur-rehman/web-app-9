6c6611546855bb7ba9c7a1939e78454ff823eecd
Jazim Abbas <stackdeveloper87@gmail.com>
2021-01-29 21:27:36 +0500

    added additional routes for sending json data ..

--- server.js
+++ server.js
@@ -14,6 +14,18 @@ app.get("/about", (req, res) => {
   res.sendFile(joinWithBasePath("/about.html"));
 });
 
+app.get("/employees", (req, res) => {
+  res.send("Getting all employees");
+});
+
+app.get("/managers", (req, res) => {
+  res.send("Getting all managers");
+});
+
+app.get("/departments", (req, res) => {
+  res.send("Getting all departments");
+});
+
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () =>
   console.log(`Express http server is listening on port ${PORT}`)
